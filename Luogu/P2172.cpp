b44c84e894966624524cd65bb08cb1a2ae92cefe
Ewig-Milan <2116813725@qq.com>
2025-07-03 09:56:40 +0800

    upd 更改算法 已AC

--- Luogu/P2172.cpp
+++ Luogu/P2172.cpp
@@ -5,88 +5,52 @@ const int N = 5100, M = 35100, INF = 1e9;
 
 int n, m, c, r, S, T;
 int id[N][N], inc;
-int cost;
 
-int h[N], e[M], v[M], ca[M], ne[M], idx;
-void add(int a, int b, int c, int d) {
-    if(a == b) a = (a << 1) - 1, b <<= 1;
-    else {
-        if(a != S) a <<= 1;
-        if(b == T) a -= 1;
-        else b = (b << 1) - 1;
-    }
-    e[idx] = b, ca[idx] = c, v[idx] = d, ne[idx] = h[a], h[a] = idx++;
-    e[idx] = a, ca[idx] = 0, v[idx] = -d, ne[idx] = h[b], h[b] = idx++;
+int h[N], e[M], ca[M], ne[M], idx;
+void add(int a, int b, int c) {
+    if(a != S && b != T) a = (a << 1) - 1, b <<= 1;
+    if(a == S) b = (b << 1) - 1;
+    if(b == T) a <<= 1;
+    e[idx] = b, ca[idx] = c, ne[idx] = h[a], h[a] = idx++;
+    e[idx] = a, ca[idx] = 0, ne[idx] = h[b], h[b] = idx++;
 }
 
-int pe[N];
-bool vis[N];
-void SPFA() {
-    memset(pe, 63, sizeof pe);
-    memset(vis, 0, sizeof vis);
+int dep[N], can[N];
+bool bfs() {
+    memset(dep, -1, sizeof dep);
     queue<int> q;
-    pe[S] = 0, q.push(S), vis[S] = 1;
+    dep[S] = 0, q.push(S);
     while(!q.empty()) {
         int x = q.front();
-        q.pop(), vis[x] = 0;
-        for(int i = h[x]; ~i; i = ne[i]) {
-            int j = e[i];
-            if(ca[i] && pe[j] > pe[x] + v[i]) {
-                pe[j] = pe[x] + v[i];
-                if(!vis[j]) q.push(j), vis[j] = 1;
-            }
-        }
-    }
-}
-
-struct Node {
-    int dis, id;
-    friend bool operator < (const Node & a, const Node & b) {return a.dis > b.dis;}
-};
-int dis[N], can[N];
-bool dij() {
-    for(int i = 0; i < N; i++) dis[i] = INF, vis[i] = 0, can[i] = h[i];
-    priority_queue<Node> q;
-    dis[S] = 0, q.push({0, S});
-    while(!q.empty()) {
-        int x = q.top().id;
         q.pop();
-        if(vis[x]) continue;
-        vis[x] = 1;
+        can[x] = h[x];
+        if(x == T) return true;
         for(int i = h[x]; ~i; i = ne[i]) {
             int j = e[i];
-            if(ca[i] && dis[j] > dis[x] + v[i] + pe[x] - pe[j]) {
-                dis[j] = dis[x] + v[i] + pe[x] - pe[j];
-                q.push({dis[j], j});
-            }
+            if(dep[j] == -1 && ca[i]) dep[j] = dep[x] + 1, q.push(j);
         }
     }
-    memset(vis, 0, sizeof vis);
-    for(int i = 0; i < N; i++) pe[i] += dis[i];
-    return dis[T] < INF;
+    return false;
 }
 
 int dfs(int x, int lim) {
     if(x == T) return lim;
-    vis[x] = 1;
     int flow = 0;
     for(int &i = can[x]; ~i && flow < lim; i = ne[i]) {
         int j = e[i];
-        if(v[i] + pe[x] - pe[j] == 0 && !vis[j] && ca[i]) {
+        if(dep[j] == dep[x] + 1 && ca[i]) {
             int tmp = dfs(j, min(ca[i], lim - flow));
-            if(!tmp) vis[j] = 1; // 此处写法正确性已验证
-            else flow += tmp, ca[i] -= tmp, ca[i ^ 1] += tmp, cost += tmp * v[i];
+            if(!tmp) dep[j] = -1;
+            else flow += tmp, ca[i] -= tmp, ca[i ^ 1] += tmp;
         }
     }
-    vis[x] = 0;
     return flow;
 }
 
-int PD() {
-    cost = 0, SPFA();
+int dinic() {
     int mf = 0, tmp;
-    while(dij()) while(tmp = dfs(S, INF)) mf += tmp;
-    return cost;
+    while(bfs()) while(tmp = dfs(S, INF)) mf += tmp;
+    return mf;
 }
 
 int main() {
@@ -99,17 +63,16 @@ int main() {
             if(ch[j] == 'x') continue;
             id[i][j] = ++inc;
             if(i - r > 0 && j - c > 0 && id[i - r][j - c])
-                add(id[i - r][j - c], inc, 1, -1);
+                add(id[i - r][j - c], inc, 1);
             if(i - r > 0 && j + c > 0 && id[i - r][j + c])
-                add(id[i - r][j + c], inc, 1, -1);
+                add(id[i - r][j + c], inc, 1);
             if(i - c > 0 && j - r > 0 && id[i - c][j - r])
-                add(id[i - c][j - r], inc, 1, -1);
+                add(id[i - c][j - r], inc, 1);
             if(i - c > 0 && j + r > 0 && id[i - c][j + r])
-                add(id[i - c][j + r], inc, 1, -1);
-            add(S, inc, INF, 0), add(inc, T, 1, 0);
-            add(inc, inc, 1, 0);
+                add(id[i - c][j + r], inc, 1);
+            add(S, inc, 1), add(inc, T, 1);
         }
     }
-    printf("%d", inc + PD());
+    printf("%d", inc - dinic());
     return 0;
 }
