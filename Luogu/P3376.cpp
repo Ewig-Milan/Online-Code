651cb47d50ef9c0d162450e4a603ea65136ef6bd
Ewig_Milan <2116813725@qq.com>
2025-07-13 21:21:47 +0800

    upd

--- Luogu/P3376.cpp
+++ Luogu/P3376.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-const int N = 110, M = 20100, INF = 1e9;
+const int N = 300, M = 10100;
+const ll INF = 1e12;
 
 int n, m, s, t;
 
-int h[N], e[M], ca[M], ne[M], idx;
-void add(int a, int b, int c) {
+int h[N], e[M], ne[M], idx;
+ll ca[M];
+void add(int a, int b, ll c) {
     e[idx] = b, ca[idx] = c, ne[idx] = h[a], h[a] = idx++;
     e[idx] = a, ca[idx] = 0, ne[idx] = h[b], h[b] = idx++;
 }
@@ -23,20 +26,20 @@ bool bfs() {
         if(x == t) return true;
         for(int i = h[x]; ~i; i = ne[i]) {
             int j = e[i];
-            if(dep[j] == -1 && ca[i]) dep[j] = dep[x] + 1, q.push(j); 
+            if(dep[j] == -1 && ca[i]) dep[j] = dep[x] + 1, q.push(j);
         }
     }
     return false;
 }
 
-int dfs(int x, int lim) {
+ll dfs(int x, ll lim) {
     if(x == t) return lim;
-    int flow = 0;
+    ll flow = 0;
     for(int i = can[x]; ~i && flow < lim; i = ne[i]) {
         can[x] = i;
         int j = e[i];
         if(dep[j] == dep[x] + 1 && ca[i]) {
-            int tmp = dfs(j, min(ca[i], lim - flow));
+            ll tmp = dfs(j, min(ca[i], lim - flow));
             if(!tmp) dep[j] = -1;
             else flow += tmp, ca[i] -= tmp, ca[i ^ 1] += tmp;
         }
@@ -44,22 +47,19 @@ int dfs(int x, int lim) {
     return flow;
 }
 
-int dinic() {
-    int mf = 0, tmp;
+ll dinic() {
+    ll mf = 0, tmp;
     while(bfs()) while(tmp = dfs(s, INF)) mf += tmp;
     return mf;
 }
 
 int main() {
     memset(h, -1, sizeof h);
-    scanf("%d%d", &m, &n);
-    s = 0, t = n + 1;
-    for(int i = 1; i <= m; i++) add(s, i, 1);
-    for(int i = m + 1; i <= n; i++) add(i, t, 1);
-    int a, b;
-    while(~scanf("%d%d", &a, &b) && a != -1) add(a, b, 1);
-    printf("%d\n", dinic());
-    for(int i = 0; i <= idx; i += 2) if(e[i] > m && e[i] <= n && !ca[i])
-        printf("%d %d\n", e[i ^ 1], e[i]);
+    scanf("%d%d%d%d", &n, &m, &s, &t);
+    for(int i = 1; i <= m; i++) {
+        int a, b; ll c; scanf("%d%d%lld", &a, &b, &c);
+        add(a, b, c);
+    }
+    printf("%lld", dinic());
     return 0;
 }
